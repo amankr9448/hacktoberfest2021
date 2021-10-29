ab81140aa5914a0ccf4a11d4766bbdb0ee94b549
amankr9448 <61510573+amankr9448@users.noreply.github.com>
2021-10-30 01:25:06 +0530

    Updated program for sum of two number in array

--- CPP/Leet Code Solutions/two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ CPP/Leet Code Solutions/two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,36 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int start = 0;
-        int end = numbers.size()-1;
-        while(start<end){
-            int sum = numbers[start] + numbers[end];
-            if(sum > target){
-                end--;
-            } else if(sum < target){
-                start++;
-            } else {
-                return vector<int>{start+1, end+1};
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cout << "enter the array size" << endl;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n;i++)
+        cin >> a[i];
+    cout << "enter the target value" << endl;
+    cin >> k;
+
+    sort(a,a+n);
+    int start = a[0];
+    int last = a[n-1];
+    int last2 = a[n - 2];
+
+    if(start>k||last+last2<k)
+        cout << "out of range" << endl;
+    else
+    {
+        for (int i = 0; i < n-1; i++)
+        {
+            for ( int j = i+1; j < n; j++)
+            {
+                if(a[i]+a[j]==k)
+                    cout << i << "," << j << endl;
             }
+            
         }
-        return vector<int>{start+1, end+1};
+        
     }
-};
+    return 0;
+}
