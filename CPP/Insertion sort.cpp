cd5409ee50dbcef68eeab84ec779f8035b750204
amankr9448 <61510573+amankr9448@users.noreply.github.com>
2021-10-30 00:23:46 +0530

    Updated Insertion sort code
    In this updated code user has the choice to choose between which operations to perform 
    Ascending or Descending

--- CPP/Insertion sort.cpp
+++ CPP/Insertion sort.cpp
@@ -1,5 +1,3 @@
-//Insertion sort
-
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -9,12 +7,17 @@ int main()
     cout<<"enter size of array :";
     cin>>s;
     int arr[s];
+    cout<<"enter the array values"<<endl;
     for(i=0;i<s;i++)
     {
         cin>>arr[i];
     }
-    
-    for(i=1;i<s;i++)
+    int choice;
+    cout<<"choose 1 for ascending operation or 2 for descending operation"<<endl;
+    cin>>choice;
+    if(choice==1)
+    {
+         for(i=1;i<s;i++)
     {
         current=arr[i];
         j=i-1;
@@ -27,6 +30,25 @@ int main()
         j++;
         arr[j]=current;
     }
+    }
+    else if(choice==2)
+    {
+         for(i=1;i<s;i++)
+    {
+        current=arr[i];
+        j=i-1;
+        
+        while(arr[j]<current&&j>=0)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        j++;
+        arr[j]=current;
+    }
+    }
+   else
+       cout<<"Invalid Choice"<<endl;
     for(i=0;i<s;i++)
     {
         cout<<arr[i]<<" ";
