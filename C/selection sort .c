d633f359e803579a8b794eb7456461cb855abf84
amankr9448 <61510573+amankr9448@users.noreply.github.com>
2021-10-29 23:09:45 +0530

    Update selection sort .c

--- C/selection sort .c
+++ C/selection sort .c
@@ -1,16 +1,23 @@
 #include<stdio.h>
 int main(void)
 {
-	int a[100],n,i,j,b;
-printf("enter the number of values to be inserted");
+	int a[100],n,i,j,b,choice;
+printf("enter the number of values to be inserted\n");
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
 	printf("enter the value");
 	scanf("%d",&a[i]);
 }
-for(i=0;i<n;i++)
+
+printf("Which Search Operation Do you want to use\n");
+printf("Click 1 for Ascending Order or 2 for Descending order\n");
+scanf("%d",&choice);
+
+if(choice==1)
 {
+	for(i=0;i<n;i++)
+        {
 	for(j=i+1;j<n;j++)
 	{
 	if(a[i]<a[j])
@@ -19,8 +26,27 @@ for(i=0;i<n;i++)
 		a[i]=a[j];
 		a[j]=b;
 	}
+       }
+      }
+}
+	
+else
+{
+	for(i=0;i<n;i++)
+{
+	for(j=i+1;j<n;j++)
+	{
+	if(a[i]>a[j])
+	{
+		b=a[i];
+		a[i]=a[j];
+		a[j]=b;
+	}
 }
 }
+}
+	
+
 for(i=0;i<n;i++)
 printf("%d ",a[i]);
 }
